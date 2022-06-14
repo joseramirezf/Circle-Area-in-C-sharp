41574bd9516f4ac55ce6665fc7f3ec3df3845c68
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-14 08:35:51 -0600

    Circle Area

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double pi = 3.14D;
+
+int radius;
+Console.WriteLine("Enter the radius of the circle");
+radius = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The area of the circle is:");
+Console.WriteLine((Math.Pow(radius, 2)) * pi);
